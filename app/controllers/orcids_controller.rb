7f09d0b304577dc86e269194936e8d257616f2cf
Bess Sadler <bess.sadler@princeton.edu>
2023-10-24 08:25:44 -0400

    Log what we get from orcid and redirect the user back to the user show screen

--- app/controllers/orcids_controller.rb
+++ app/controllers/orcids_controller.rb
@@ -9,5 +9,8 @@ class OrcidsController < ApplicationController
     omniauth = request.env["omniauth.auth"]
     session[:omniauth] = omniauth
     session[:params]   = params
+    Rails.logger.info("omniauth: #{omniauth.inspect}")
+    Rails.logger.info("params: #{params.inspect}")
+    redirect_to user_show_path(current_user)
   end
 end
