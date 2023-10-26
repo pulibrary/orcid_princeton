22d28131c8008b54103fe5ca3ceb2214e9aaeb41
Carolyn Cole <cac9@princeton.edu>
2023-10-26 14:07:50 -0400

    Putting the image into the vite path and utilizing it from there

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,5 @@ module OrcidPrinceton
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.assets.enabled = true
   end
 end
