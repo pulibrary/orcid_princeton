e565af939123a6a2c8499fa2592174179133119b
Bess Sadler <bess.sadler@princeton.edu>
2023-10-31 14:32:01 -0400

    Add a configurable banner for annoucements (#91)
    Warn users that for now this application only connects to the ORCID
    sandbox, not the real ORCID API

---
+++ spec/system/banner_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+describe "Website banner", type: :system, js: true do
+  let(:user) { FactoryBot.create :user }
+  it "has the banner on the homepage" do
+    visit "/"
+    expect(page).to have_css "#banner"
+  end
+
+  it "renders html tags in the banner" do
+    visit "/"
+    expect(page).not_to have_content "<i>test</i>"
+    expect(page.find("div#banner h1 i").text).to eq "test"
+    expect(page).not_to have_content "<b>test</b>"
+    expect(page.find("div#banner p b").text).to eq "test"
+    expect(page).to have_link "message", href: "mailto:fake@princeton.edu"
+  end
+end
