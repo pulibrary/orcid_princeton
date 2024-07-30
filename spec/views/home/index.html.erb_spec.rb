a3e23c1ca77b5428cef3bdbcec45b0b16b9fb9eb
Lauren Malek <etoile22lm@gmail.com>
2024-07-30 12:25:08 -0400

    changed welcome message

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe "home/index.html.erb", type: :view do
+RSpec.feature "home/index.html.erb", type: ["feature", "system"] do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "displays correct homepage message" do
+    let(:user) { FactoryBot.create :user_with_orcid_and_token }
+
+    it "displays prompt to log in message" do
+      visit "/home/index" # root
+      expect(page).to have_content("Connect your ORCID id to Princeton so that published works can be easily identified as belonging to all Princeton researchers.")
+      expect(page).to have_content("Log-in")
+    end
+
+    it "displays already signed in message" do
+      login_as user
+      visit "/home/index" # root
+      expect(page).to have_content("Hello, " + user.given_name + "!")
+      expect(page).to have_content("Go to your Profile Page to view your account.")
+      expect(page).to have_link "Profile Page", href: "/users/" + user.id.to_s
+      expect(page).to have_no_content("Log-in")
+    end
+  end
 end
