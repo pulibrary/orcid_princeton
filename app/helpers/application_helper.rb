686d7f8de5fe0f443a1a1e501f157598886a4d3f
Kate Lynch <katelynch@princeton.edu>
2024-07-31 09:36:47 -0400

    Refactored partial rendering logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 # frozen_string_literal: true
 module ApplicationHelper
-  def plausible_partial
-    return "shared/plausible_dev_staging" if Rails.env.development? || Rails.env.staging?
-    return "shared/plausible" if Rails.env.production?
-  end
 end
