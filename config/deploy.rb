c33a3322d98584e24e523b103b71902119d00853
Carolyn Cole <cac9@princeton.edu>
2024-08-08 09:34:11 -0400

    Linking vite assets in shared directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.18.0"
 set :application, "orcid_princeton"
 set :repo_url, "https://github.com/pulibrary/orcid_princeton.git"
 
-set :linked_dirs, %w[log public/system public/assets node_modules]
+set :linked_dirs, %w[log public/system public/vite node_modules]
 
 # Default branch is :main
 set :branch, ENV["BRANCH"] || "main"
@@ -20,3 +20,5 @@ Rake::Task["deploy:assets:backup_manifest"].clear_actions
 Rake::Task["deploy:assets:restore_manifest"].clear_actions
 
 before "deploy:reverted", "deploy:assets:precompile"
+
+before "deploy:compile_assets", "deploy:clobber_assets"
