e1180339c7f9d007fb40ec2c43bfc494da60f973
Kate Lynch <katelynch@princeton.edu>
2024-08-22 10:28:59 -0400

    Added vite tag helpers

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 
 RSpec.describe UsersHelper, type: :helper do
   describe "#full_orcid_display" do
+    include ViteRails::TagHelpers
+
     user_without_orcid = User.create(uid: "abc")
     user_with_orcid = FactoryBot.create(:user_with_orcid_and_token)
 
@@ -10,8 +12,8 @@ RSpec.describe UsersHelper, type: :helper do
       expect(full_orcid_display(user_without_orcid.orcid)).to eq(nil)
     end
 
-    it "returns ORCID information if the user has an ORCID iD" do
-      expect(full_orcid_display(user_with_orcid.orcid)).to contain("<a href=\"https://orcid.org/0000-0015-0000-0088\">https://orcid.org/0000-0015-0000-0088</a>")
+    it "returns an ORCID URL if the user has an ORCID iD" do
+      expect(full_orcid_display(user_with_orcid.orcid)).to include("<a href=\"https://orcid.org/0000-0015-0000-0088\">https://orcid.org/0000-0015-0000-0088</a>")
     end
   end
 end
