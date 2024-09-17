35279c538b7abfa9b64e7776035266413442bfb3
Hector Correa <hector_correa@princeton.edu>
2024-09-17 14:14:32 -0400

    Added a timeout to the ORCID check

--- app/models/orcid_api_status.rb
+++ app/models/orcid_api_status.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 class OrcidApiStatus < HealthMonitor::Providers::Base
   def check!
-    uri = URI("https://api.orcid.org/v3.0/apiStatus")
-    response = Net::HTTP.get(uri)
-    json = JSON.parse(response)
-    if json.values.include?(false)
-      raise "The ORCID API has an invalid status https://api.orcid.org/v3.0/apiStatus"
+    Net::HTTP.start("https://api.orcid.org") do |http|
+      http.read_timeout = 2  # seconds
+      response = http.get("/v3.0/apiStatus")
+      json = JSON.parse(response)
+      if json.values.include?(false)
+        raise "The ORCID API has an invalid status https://api.orcid.org/v3.0/apiStatus"
+      end
     end
   end
 end
