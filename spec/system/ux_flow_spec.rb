e3200e8bafe1eeb3bf4794798038afee4ecf0444
Kate Lynch <katelynch@princeton.edu>
2025-03-05 16:10:18 -0500

    Fixed LUX header and updated spec

--- spec/system/ux_flow_spec.rb
+++ spec/system/ux_flow_spec.rb
@@ -19,9 +19,9 @@ describe "user experience from start to finish", type: :system, js: true do
       # The page should be accessible.
       expect(page).to be_axe_clean
         .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508, :"best-practice")
-      expect(page).to have_content(user.display_name)
+      expect(page).to have_content(user.uid)
       # Expand the user menu dropdown
-      click_on user.display_name.to_s
+      click_on user.uid.to_s
       click_on "Profile"
 
       # The user is redirected to the user page after logging in.
